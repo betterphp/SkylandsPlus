58078fffc05ff9f0327b231a2a716d1886819b59
Jacek <jacek@kuzemczak.co.uk>
2012-04-09 14:13:58 +0100

    Made jungles more jungly

--- uk/co/jacekk/bukkit/SkylandsPlus/generation/TreePopulator.java
+++ uk/co/jacekk/bukkit/SkylandsPlus/generation/TreePopulator.java
@@ -51,7 +51,7 @@ public class TreePopulator extends BlockPopulator {
 							}else if (biome == Biome.JUNGLE || biome == Biome.JUNGLE_HILLS){
 								if (this.random.nextInt(1000) < 75){
 									world.generateTree(block.getLocation(), TreeType.JUNGLE);
-								}else if (this.random.nextInt(1000) < 150){
+								}else if (this.random.nextInt(1000) < 300){
 									world.generateTree(block.getLocation(), TreeType.JUNGLE_BUSH);
 								}
 							}else if (biome != Biome.DESERT && biome != Biome.DESERT_HILLS){
