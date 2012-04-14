8968c12b7a3d2e7aa829e81e522ec9465f2f5142
Xiol <dane.elwell@gmail.com>
2012-04-14 02:09:26 +0200

    The message "Set the world type..." wasn't clear on whether it was asking me to set the type or that it had set the type. Had to come browser the source to figure it out.

--- uk/co/jacekk/bukkit/SkylandsPlus/listeners/WorldListener.java
+++ uk/co/jacekk/bukkit/SkylandsPlus/listeners/WorldListener.java
@@ -38,7 +38,7 @@ public class WorldListener implements Listener {
 				
 				type.set(worldServer.worldData, WorldType.NORMAL);
 				
-				plugin.log.info("Set the world type of '" + world.getName() + "' to normal (this is required for custom biomes to work).");
+				plugin.log.info("We have set the world type of '" + world.getName() + "' to normal (this is required for custom biomes to work).");
 			}catch (Exception e){
 				plugin.log.info("Could not change the world type of '" + world.getName() + "'.");
 				e.printStackTrace();
