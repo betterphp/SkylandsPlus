3660a11c7b98ae6f129a28812b48ffdab719a043
Hummtaro <NicholasR@hotmail.de>
2013-07-03 08:43:09 +0200

    adds possibility to use parameters. remove worldlistener.

--- src/main/java/uk/co/jacekk/bukkit/skylandsplus/SkylandsPlus.java
+++ src/main/java/uk/co/jacekk/bukkit/skylandsplus/SkylandsPlus.java
@@ -10,10 +10,9 @@ import uk.co.jacekk.bukkit.skylandsplus.listeners.MobSpawnListener;
 import uk.co.jacekk.bukkit.skylandsplus.listeners.PhysicsListener;
 
 public class SkylandsPlus extends BasePlugin {
-	public boolean generator = false;
 	public void onEnable(){
 		super.onEnable(true);
-		
+
 		this.config = new PluginConfig(new File(this.baseDirPath + File.separator + "config.yml"), Config.class, this.log);
 		
 		if (this.config.getBoolean(Config.PREVENT_SAND_FALLING)){
